3c0bf22cd479ae13befbadd23ae6bee9ed817972
Hirokazu Matsmoto <matsmoto@msf.biglobe.ne.jp>
2016-09-17 04:26:18 +0900

    Initial Commit

---
+++ pockedex3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pockedex3
+//
+//  Created by 松本広一 on 2016/09/17.
+//  Copyright © 2016年 和光ファーム. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
